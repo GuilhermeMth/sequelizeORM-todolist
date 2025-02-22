410d811695b80d8e6023323ed675ce7b00025727
GuilhermeMthDev <Guilherme2006bonitao@Gmail.com>
2025-02-22 18:03:26 -0300

    feat: adicionando o codigo 404 do status http

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const app = express();
 const todoRoutes = require('./routes/todoRoutes');
 app.use('/todo', todoRoutes);
 
+app.use('*', (req, res, next) => {
+    res.status(404).json({
+        status: "error",
+        message: "Route not found"
+    });
+});
+
 app.get('/', (req, res) => {
     res.status(200).json({
         status: "success",
         message: "API is running"
-    })
+    });
 });
 
 const PORT = process.env.APP_PORT || 3000;
