b9e9c782ecccd958ae0cc16dbbe5121c81898462
GuilhermeMth <Guilhermemfranca06@gmail.com>
2025-02-24 22:45:07 -0300

    chore: configurando e criando o DB com o config.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: '${process.cwd()}/.env'});
+require('dotenv').config({path: `${process.cwd()}/.env`});
 const express = require('express');
 const app = express();
 
--- config/config.js
+++ config/config.js
@@ -1,23 +1,25 @@
-module.exorts = {
+require('dotenv').config({path: `${process.cwd()}/.env`});
+
+module.exports = {
     development: {
-      username: "root",
-      password: null,
-      database: "database_development",
-      host: "127.0.0.1",
-      dialect: "mysql"
+      username: process.env.DB_USERNAME, 
+      password: process.env.DB_PASSWORD, 
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT 
     },
     test: {
-      username: "root",
-      password: null,
-      database: "database_test",
-      host: "127.0.0.1",
-      dialect: "mysql"
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME, 
+      host: process.env.DB_HOST, 
+      dialect: process.env.DB_DIALECT 
     },
     production: {
-      username: "root",
-      password: null,
-      database: "database_production",
-      host: "127.0.0.1",
-      dialect: "mysql"
+      username: process.env.DB_USERNAME, 
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME, 
+      host: process.env.DB_HOST, 
+      dialect: process.env.DB_DIALECT 
     },
 };
