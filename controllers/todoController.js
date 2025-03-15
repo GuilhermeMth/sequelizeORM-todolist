613134420e9794672241c9d375af056885203f17
GuilhermeMth <Guilhermemfranca06@gmail.com>
2025-03-14 22:21:32 -0300

    feat: adicionando a tabela da task em todoController

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,5 +1,26 @@
 const todo = (req, res, next) => {
     const body = req.body;
+
+    const newTask = {
+        id: 1,
+        title: body.title,
+        status: body.status,
+        priority: body.priority,
+        description: body.description
+    };
+
+    if (!newTask) {
+        return res.status(400).json({
+            status: "error",
+            message: "Task not created"
+        });
+    }
+
+    return res.status(201).json({
+        status: "success",
+        message: "Task created",
+        data: newTask
+    });
 };
 
 module.exports = { todo };
